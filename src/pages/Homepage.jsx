60a01b7f123f496a420b1a3edfa4b0ae693d0202
Alvin0820 <your-email@example.com>
2026-07-23 13:46:23 -0400

    Fix the file changes and Add HomePage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import PollCard from "../components/pollCard";
+
+function Home() {
+  const [polls, setPolls] = useState([]);
+
+  useEffect(() => {
+    fetchPolls();
+  }, []);
+
+  async function fetchPolls() {
+    try {
+      const response = await fetch("http://localhost:8080/polls");
+      const data = await response.json();
+      setPolls(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Polling App</h1>
+
+      {polls.map((poll) => (
+        <PollCard key={poll.id} poll={poll} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
+            
+
+
+
+
+
+
+
+
